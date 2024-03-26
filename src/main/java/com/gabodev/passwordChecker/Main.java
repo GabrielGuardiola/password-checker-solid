9eb0963d194b5be3563d800534f6fa26aa1970e3
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-03-26 13:17:18 +0100

    Initial Commit

---
+++ src/main/java/com/gabodev/passwordChecker/Main.java
@@ -0,0 +1,12 @@
+package com.gabodev.passwordChecker;
+
+import com.gabodev.passwordChecker.application.PasswordApplication;
+
+public class Main {
+
+    public static void main (String[] args){
+
+        PasswordApplication passwordApplication = new PasswordApplication();
+        passwordApplication.run();
+    }
+}
---
+++ src/main/java/com/gabodev/passwordChecker/application/PasswordApplication.java
@@ -0,0 +1,8 @@
+package com.gabodev.passwordChecker.application;
+
+public class PasswordApplication {
+
+    public void run(){
+        System.out.println("test");
+    }
+}
